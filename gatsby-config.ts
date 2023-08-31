efc2b877b5196e34cafd589b8cded33eb416cd07
Jay Pinho <jaypinho@gmail.com>
2023-08-31 20:53:20 +0300

    Fix metadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,14 +8,14 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
-    siteImage: `/banner.jpg`,
+    siteTitle: `Jay Pinho`,
+    siteTitleAlt: `Jay Pinho`,
+    siteHeadline: `Jay Pinho`,
+    siteUrl: `https://www.jaypinho.com/`,
+    siteDescription: `Welcome to jaypinho.com.`,
+    siteImage: `/banner.png`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `@jaypinho`,
   },
   trailingSlash: `always`,
   plugins: [
